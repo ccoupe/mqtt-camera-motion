51d289faa91e6f69917aa8aff28eff25f1779c52
Cecil <ccoupe@cableone.net>
2021-04-03 19:03:16 -0600

    another file.

--- rtsp.py
+++ rtsp.py
@@ -3,7 +3,6 @@ import cv2
 import os
 
 print (cv2.__version__)
-os.environ["OPENCV_FFMPEG_CAPTURE_OPTIONS"] = "rtsp_transport;udp"
 
 #gst = "rtspsrc location=rtsp://ccoupe:tssgnu@192.168.1.27/live latency=0 ! rtph265depay ! h265parse ! omxh265dec ! videoconvert ! appsink"
 #
@@ -20,8 +19,30 @@ gst = "rtspsrc location=rtsp://ccoupe:tssgnu@192.168.1.27/live latency=0 ! rtph2
 #
 #gst = "rtspsrc location='rtsp://ccoupe:tssgnu@192.168.1.27/live' latency=0 buffer-mode=auto ! decodebin ! videoconvert ! appsink"
 
-cap = cv2.VideoCapture(gst)
-#cap = cv2.VideoCapture("rtsp://ccoupe:tssgnu@192.168.1.27/live", cv2.CAP_FFMPEG)
+def nvidia_cam_rtsp(uri, width, height, latency):
+    gst_str = ("rtspsrc location={} latency={} ! rtph264depay ! h264parse ! omxh264dec ! "
+               "nvvidconv ! video/x-raw, width=(int){}, height=(int){}, format=(string)BGRx ! "
+               "videoconvert ! appsink").format(uri, latency, width, height)
+    return cv2.VideoCapture(gst_str, cv2.CAP_GSTREAMER)
+    
+def open_cam_rtsp(uri, width, height, latency):
+  os.environ["OPENCV_FFMPEG_CAPTURE_OPTIONS"] = "rtsp_transport;udp"
+  return cv2.VideoCapture(uri, cv2.CAP_FFMPEG)
+  
+def open_cam_gst(uri, width, height, latency):
+  pass
+
+def open_cam_bare(uri, width, height, latency):
+    return cv2.VideoCapture(uri)
+
+uri = 'rtsp://ccoupe:tssgnu@192.168.1.40/live'  # Wyze V2 aka patio
+#uri = 'rtsp://ccoupe:tssgnu@192.168.1.28/live'  # Wyze Pan aka mantle
+
+#cap = nvidia_cam_rtsp(uri, 640, 360, 0)
+#cap = open_cam_rtsp(uri, 640, 360, 0)
+cap = open_cam_bare(uri, 640, 360, 0)
+
+
 if not cap.isOpened() :
         print("capture failed")
         exit()
