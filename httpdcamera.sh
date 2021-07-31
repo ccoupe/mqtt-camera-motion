75af3d799b72771140ef02223b60891bb8e14c9c
Cecil <ccoupe@cableone.net>
2021-07-31 13:44:08 -0600

    begin cleanup of unused/unwanted files.

--- httpdcamera.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-/usr/bin/python3 -m http.server 7534 --bind 192.168.1.25 --directory /var/www &
--- restart/mqttrestart.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-mosquitto_pub -h 192.168.1.7 -t network/restart -m `hostname`
